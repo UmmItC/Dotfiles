0f1d653bc14621cf18c5a419e8a8cc7a7078a8fa
UmmIt <git@ummit.dev>
2024-06-03 23:17:43 +0800

    refactor: Improve script modularity, formatting and color usage
    * Better modularity and readability
    * consistent formatting and indentation
    * Improved color usage for clarity and readability

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Define colors
+readonly COLOR_YELLOW=$(tput setaf 3)
+readonly COLOR_GREEN=$(tput setaf 2)
+readonly COLOR_GREY=$(tput setaf 8)
+readonly COLOR_DARK_RED=$(tput setaf 1)
+readonly COLOR_RESET=$(tput sgr0)
+
 # Function to prompt for Yes/No
 prompt_yna() {
+    local choice
     while true; do
-        read -rp "$(tput setaf 3)$1 (Y/N): $(tput sgr0)" choice
+        read -rp "${COLOR_GREEN}$1 (Y/N): ${COLOR_RESET}" choice
         case $choice in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer Y or N.";;
+            [Yy]*) return 0 ;;
+            [Nn]*) return 1 ;;
+            *) echo "$COLOR_DARK_RED:: Please answer Y or N.$COLOR_RESET" ;;
         esac
     done
 }
@@ -15,6 +23,88 @@ prompt_yna() {
 # Function to check if a package is installed
 is_package_installed() {
     pacman -Qs "$1" &>/dev/null
+}
+
+# Function to install Pacman packages
+install_pacman_packages() {
+    local pacman_packages=("neofetch" "zsh" "hyprland" "hyprpaper" "waybar" "fuzzel" "ttf-jetbrains-mono" "kitty" "git" "cliphist" "clipmenu" "neovim" "hyprlock")
+    echo "${COLOR_GREEN}:: Pacman packages to be installed:${COLOR_RESET}"
+    for package in "${pacman_packages[@]}"; do
+        echo "${COLOR_GREY}$package${COLOR_RESET}"
+    done
+    if prompt_yna ":: Install these pacman packages?"; then
+        sudo pacman -S "${pacman_packages[@]}"
+    fi
+}
+
+# Function to install aur packages
+install_aur_packages() {
+    local aur_packages=("wlogout" "hyprshot")
+    echo "${COLOR_GREEN}:: AUR packages to be installed:${COLOR_RESET}"
+    for package in "${aur_packages[@]}"; do
+        echo "${COLOR_GREY}$package${COLOR_RESET}"
+    done
+    if prompt_yna ":: Install these aur packages?"; then
+        sudo pacman -S "${aur_packages[@]}"
+    fi
+}
+
+# Copy configuration files
+config_dir="$HOME/.config"
+
+# Function to copy configuration files
+copy_config_files() {
+    if prompt_yna ":: Copy configuration files?"; then
+        cp -rv ./hypr "$config_dir/"
+        cp -rv ./kitty "$config_dir/"
+        cp -rv ./neofetch "$config_dir/"
+        cp -rv ./waybar "$config_dir/"
+        cp -v .p10k.zsh "$config_dir/"
+        cp -v .zshrc "$config_dir/"
+    else
+        echo "${COLOR_YELLOW}:: Skipping configuration file copying.${COLOR_RESET}"
+    fi
+}
+
+# Main function
+main() {
+    # Check if a resolution is selected
+    if [[ -z $selected_resolution ]]; then
+        echo "${COLOR_DARK_RED}:: No resolution selected. Exiting...${COLOR_RESET}"
+        exit 1
+    else
+        echo "${COLOR_GREEN}:: You selected $selected_resolution as your screen resolution. ($connected_display)${COLOR_RESET}"
+    fi
+
+    install_pacman_packages
+    install_aur_packages
+    copy_config_files
+
+    # Prompt for monitor refresh rate until a valid integer is provided
+    while true; do
+        read -rp ":: Enter the Monitor refresh rate you want: " monitor_hz
+        if [[ -z $monitor_hz ]]; then
+            echo "${COLOR_YELLOW}:: Warning - Input cannot be empty.${COLOR_RESET}"
+        elif ! [[ $monitor_hz =~ ^[0-9]+$ ]]; then
+            echo "${COLOR_DARK_RED}:: Error - '$monitor_hz' is not a valid integer.${COLOR_RESET}"
+        else
+            break
+        fi
+    done
+
+    # Update hyprland.conf on line 16
+    sed -i "16 s/^monitor=.*/monitor=$connected_display,$selected_resolution@$monitor_hz,0x0,1/" "$config_dir/hypr/hyprland.conf"
+
+    # Prompt for cloning NvChad and running nvim
+    if prompt_yna ":: Clone NvChad and run nvim?"; then
+        echo "${COLOR_YELLOW}Neovim using NvChad:${COLOR_RESET}"
+        echo ":: NvChad will ask you the default configuration, please enter 'n'."
+        git clone https://github.com/NvChad/NvChad "$config_dir/nvim" --depth 1 && nvim
+    else
+        echo "${COLOR_YELLOW}:: Skipping NvChad cloning and nvim execution.${COLOR_RESET}"
+    fi
+
+    echo "${COLOR_GREEN}:: Installation completed successfully.${COLOR_RESET}"
 }
 
 # Get the connected display name
@@ -52,80 +142,4 @@ selected_resolution=$(whiptail --title "Resolution Selection" --menu "Choose a r
 "640x350" "" \
 3>&1 1>&2 2>&3)
 
-# Check if the user cancelled or exited without making a selection
-if [[ $? -ne 0 ]]; then
-    echo "No resolution selected. Exiting..."
-    exit 1
-else
-  echo "You selected $selected_resolution as your screen resolution. ($connected_display)"
-fi
-
-
-# List and install necessary pacman packages
-pacman_packages=("neofetch" "zsh" "hyprland" "hyprpaper" "waybar" "fuzzel" "ttf-jetbrains-mono" "kitty" "git" "cliphist" "clipmenu" "neovim" "hyprlock")
-
-echo "$(tput setaf 6)Pacman packages to be installed:$(tput sgr0)"
-printf "$(tput setaf 4)%s$(tput sgr0)\n" "${pacman_packages[@]}"
-if prompt_yna "Install these pacman packages?"; then
-    sudo pacman -S "${pacman_packages[@]}"
-fi
-
-# Install yay if not installed
-if ! is_package_installed "yay"; then
-    git clone https://aur.archlinux.org/yay.git
-    cd yay || exit
-    makepkg -si
-    cd ..
-    rm -rf yay
-fi
-
-# List and install necessary yay packages
-yay_packages=("wlogout" "hyprshot")
-
-echo "$(tput setaf 6)Yay packages to be installed:$(tput sgr0)"
-printf "$(tput setaf 4)%s$(tput sgr0)\n" "${yay_packages[@]}"
-if prompt_yna "Install these yay packages?"; then
-    yay -S "${yay_packages[@]}"
-fi
-
-# Copy configuration files
-config_dir="$HOME/.config"
-
-# Prompt for copying configurations
-if prompt_yna "Copy configuration files?"; then
-    cp -rv ./hypr "$config_dir/"
-    cp -rv ./kitty "$config_dir/"
-    cp -rv ./neofetch "$config_dir/"
-    cp -rv ./waybar "$config_dir/"
-    cp -v .p10k.zsh "$config_dir/"
-    cp -v .zshrc "$config_dir/"
-else
-    echo "$(tput setaf 3)Skipping configuration file copying.$(tput sgr0)"
-fi
-
-# Prompt for monitor refresh rate until a valid integer is provided
-while true; do
-    read -rp "Enter the Monitor refresh rate you want: " monitor_hz
-    if [[ -z $monitor_hz ]]; then
-        echo "$(tput setaf 3):: Warning - Input cannot be empty.$(tput sgr0)"
-    elif ! [[ $monitor_hz =~ ^[0-9]+$ ]]; then
-        echo "$(tput setaf 1):: Error - '$monitor_hz' is not a valid integer.$(tput sgr0)"
-    else
-        break
-    fi
-done
-
-# Update hyprland.conf on line 16
-sed -i "16 s/^monitor=.*/monitor=$connected_display,$selected_resolution@$monitor_hz,0x0,1/" "$config_dir/hypr/hyprland.conf"
-
-# Prompt for cloning NvChad and running nvim
-if prompt_yna "Clone NvChad and run nvim?"; then
-    echo "$(tput setaf 6)Neovim using NvChad:$(tput sgr0)"
-    echo "NvChad will ask you the default configuration, please enter 'n'."
-    git clone https://github.com/NvChad/NvChad "$config_dir/nvim" --depth 1 && nvim
-else
-    echo "$(tput setaf 3)Skipping NvChad cloning and nvim execution.$(tput sgr0)"
-fi
-
-echo "$(tput setaf 2)Installation completed successfully.$(tput sgr0)"
-
+main
