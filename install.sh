6613dfa536f442def89b751a10410953dadea835
UmmIt <git@ummit.dev>
2024-06-11 01:54:02 +0800

    feat: Add pulseaudio packages into installation script

--- install.sh
+++ install.sh
@@ -50,21 +50,22 @@ check_yay() {
 # Function to install Pacman packages
 install_pacman_packages() {
     local pacman_terminal_packages=("zsh" "kitty")
-    local pacman_editor=("neovim" "vim")
+    local pacman_editor_packages=("neovim" "vim")
     local pacman_clipboard_packages=("cliphist" "clipmenu")
     local pacman_display_manager_package=("ly")
     local pacman_hypr_packages=("hyprland" "hyprpaper" "hyprlock")
     local pacman_fonts_packages=("ttf-jetbrains-mono" "ttf-jetbrains-mono-nerd")
     local pacman_utilities_packages=("waybar" "fuzzel" "fastfetch" "wf-recorder" "swaync")
+    local pacman_audio_packages=("pulseaudio" "pavucontrol" "lib32-alsa-plugins" "lib32-alsa-lib" "alsa-plugins")
     
     echo "${COLOR_GREEN}:: Pacman packages to be installed:${COLOR_RESET}"
 
-    for package in "${pacman_terminal_packages[@]}" "${pacman_editor[@]}" "${pacman_clipboard_packages[@]}" "${pacman_display_manager_package[@]}" "${pacman_fonts_packages[@]}" "${pacman_hypr_packages[@]}" "${pacman_utilities_packages[@]}"; do
+    for package in "${pacman_terminal_packages[@]}" "${pacman_editor[@]}" "${pacman_clipboard_packages[@]}" "${pacman_display_manager_package[@]}" "${pacman_fonts_packages[@]}" "${pacman_hypr_packages[@]}" "${pacman_utilities_packages[@]}" "${pacman_audio_packages[@]}"; do
         echo "${COLOR_GREY}$package${COLOR_RESET}"
     done
 
     if prompt_yna ":: Install these pacman packages?"; then
-        sudo pacman -S "${pacman_terminal_packages[@]}" "${pacman_fonts_packages[@]}" "${pacman_hypr_packages[@]}" "${pacman_utilities_packages[@]}"
+        sudo pacman -S "${pacman_terminal_packages[@]}" "${pacman_fonts_packages[@]}" "${pacman_hypr_packages[@]}" "${pacman_utilities_packages[@]}" "${pacman_audio_packages}"
     fi
 }
 
