247b81128232d672b2660544c594d9deceadd5d3
UmmIt <git@ummit.dev>
2024-06-21 17:35:32 +0800

    feat: Add notifications for wf-recorder when start and end record
    - Implement notifications using notify-send in the wf-recorder script.
    - Notifications inform the user about recording start and end events.
    - Display file path when recording ends.
    - Correct grammar

--- swaync/script/wf-recorder.sh
+++ swaync/script/wf-recorder.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 record_or_stop() {
-    recording_dir="/home/$USER/Videos/wf-recording"
+    recording_dir="/home/$USER/Videos/wf-recorder"
 
-    # Check if the directory exists, if not, create it
+    # Check if the directory exists; if not, create it
     if ! ls "$recording_dir" > /dev/null 2>&1; then
         mkdir -p "$recording_dir"
     fi
 
     # Check if the directory creation was successful
     if [[ ! -d $recording_dir ]]; then
-        echo "Error: Failed to create directory $recording_dir"
+        echo "Error: Failed to create the directory $recording_dir"
         exit 1
     fi
 
     if [[ $SWAYNC_TOGGLE_STATE == true ]]; then
-        wf-recorder -a --file "$recording_dir/wf-recording-$(date +'%Y-%m-%d-%H-%M-%S').mkv"
+        filename="$recording_dir/wf-recorder-$(date +'%Y-%m-%d-%H-%M-%S').mkv"
+        notify-send "wf-recorder" "Starting video recording with wf-recorder" \
+        --app-name="wf-recorder"
+        wf-recorder -a --file "$filename"
     else
         # Get the PID of wf-recorder
         PID=$(pidof wf-recorder)
         if [ -n "$PID" ]; then
             echo "Sending Ctrl + C signal to wf-recorder with PID $PID"
             kill -SIGINT $PID
+            filename=$(ls -t "$recording_dir" | head -n1)
+            notify-send "wf-recorder" "Video recording ended and saved to: \n\n$recording_dir/$filename" \
+            --app-name="wf-recorder"
+            echo "Video recording ended and saved to $filename"
         else
             echo "wf-recorder is not running."
         fi
