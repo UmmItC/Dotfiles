aec8d70683b7f7802628b5d0cd075177b13da5e0
UmmIt <git@ummit.dev>
2024-11-07 22:48:11 +0800

    feat(hyprland): add laptop-specific configurations for control and script installation
    - Configure keybindings for volume control using wpctl
    - Configure keybindings for media control using playerctl
    - Script for installing these packages

--- install/laptop.sh
+++ install/laptop.sh
@@ -3,32 +3,34 @@
 # Load the color codes and prompt functions
 source <(head -n 23 ../install.sh)
 
+# Function to display the banner
+display_banner() {
+    echo -e "${COLOR_GRAY}---------------------------------------------------------${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}Installing Brightness Control Tool (Simulated Failure)${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}This will install the following packages:${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}  - brightnessctl: A tool to manage screen brightness.${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}  - playerctl: A command-line utility to control media players (e.g., pause, play, skip tracks).${COLOR_RESET}"
+    echo -e "${COLOR_GRAY}---------------------------------------------------------${COLOR_RESET}"
+}
+
 # Function to install Pacman packages
-install_brightnessctl() {
-    local package="brightnessctl"
+install_packages() {
+    local packages=("brightnessctl" "playerctl")
     
-    echo -e "${COLOR_GREEN}:: Package to be installed: ${COLOR_RESET}$package"
+    echo -e "${COLOR_GREEN}:: Packages to be installed: ${COLOR_RESET}${packages[*]}"
 
-    if prompt_yna ":: Install $package?"; then
-        sudo pacman -S "$package"
+    if prompt_yna ":: Install ${packages[*]}?"; then
+        sudo pacman -S ${packages[*]}
 
+        # Sleep and clear to simulate failure
         sleep 1
         clear
     else
-        echo "${COLOR_YELLOW}:: Skipping $package installation.${COLOR_RESET}"
+        echo -e "${COLOR_YELLOW}:: Skipping ${packages[*]} installation.${COLOR_RESET}"
         
         sleep 1
         clear
     fi
-}
-
-# Function to display the banner
-display_banner() {
-    echo -e "${COLOR_GRAY}---------------------------------------------------------${COLOR_RESET}"
-    echo -e "${COLOR_BLUE}Installing Brightness Control Tool${COLOR_RESET}"
-    echo -e "${COLOR_BLUE}This will install brightnessctl for managing screen brightness.${COLOR_RESET}"
-    echo -e "${COLOR_BLUE}You will be prompted to choose whether to proceed with the installation.${COLOR_RESET}"
-    echo -e "${COLOR_GRAY}---------------------------------------------------------${COLOR_RESET}"
 }
 
 main() {
@@ -36,8 +38,8 @@ main() {
     # Display the banner
     display_banner
 
-    # Install brightnessctl
-    install_brightnessctl
+    # Install brightnessctl and playerctl
+    install_packages
 }
 
 # Run the main function
