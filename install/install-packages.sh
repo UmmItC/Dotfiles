d119d3a4594ccfa87e88b3a8dcb9b076809dc698
UmmIt <git@ummit.dev>
2025-01-08 03:22:49 +0800

    feat(install): read package names from files
    Install package will depend on `./install/packages_main` and
    `./install/packages_gpu` file.
    
    refactor(install): improve package installation logic
    
    - Updated the install_main_packages function to read package names from the packages_main file, skipping empty lines.
    - Refactored the install_gpu_package function to read GPU package names from the packages_gpu file, also skipping empty lines.
    - Removed the AUR package installation section as it is no longer needed.
    - Pacman and yay are no longer used. Use paru to do all things.

--- install/install-packages.sh
+++ install/install-packages.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 
-# Function to install Pacman packages
-install_pacman_packages() {
-    local packages=(
-        "zsh" "kitty"
-        "neovim" "vim"
-        "cliphist" "clipmenu"
-        "ly"
-        "yazi"
-        "hyprland" "hyprlock" "hypridle" "xdg-desktop-portal-hyprland"
-        "ttf-jetbrains-mono" "ttf-jetbrains-mono-nerd" "ttf-overpass" "gnu-free-fonts"
-        "papirus-icon-theme" "orchis-theme"
-        "waybar" "rofi" "fastfetch" "wf-recorder" "swaync"
-        "pipewire-jack" "pipewire-alsa" "pipewire-pulse" "qjackctl" "pavucontrol" "wireplumber"
-        "mpv"
-        "imagemagick" "grim"
-    )
+# Function to install main packages
+install_main_packages() {
+    local main_packages=()
 
-    local total_packages=${#packages[@]}
-    echo -e "${COLOR_GREEN}:: Pacman packages to be installed - Package (${total_packages})${COLOR_RESET}"
+    # Read packages from the packages_main file, skipping empty lines
+    while IFS= read -r package; do
+        # Check if the line is not empty
+        if [[ -n "$package" ]]; then
+            main_packages+=("$package")
+        fi
+    done < "./install/packages_main"
 
-    for package in "${packages[@]}"; do
-        echo -e "${COLOR_GREY}$package${COLOR_RESET}"
-    done
+    echo "${COLOR_GREEN}:: Main packages to be installed${COLOR_RESET}"
 
-    if prompt_yna ":: Install these pacman packages?"; then
-        sudo pacman -S "${packages[@]}"
+    local total_main_packages=${#main_packages[@]}
+    printf "${COLOR_GREY}%s${COLOR_RESET}\n" "${main_packages[@]}" | column
+
+    if prompt_yna ":: Install these packages? - Total Package (${total_main_packages})"; then
+        paru -S "${main_packages[@]}"
 
         sleep 1
         clear
     else
-        echo "${COLOR_YELLOW}:: Skipping pacman package installation.${COLOR_RESET}"
+        echo "${COLOR_YELLOW}:: Skipping main package installation.${COLOR_RESET}"
         
         sleep 1
         clear
@@ -39,23 +32,25 @@ install_pacman_packages() {
 
 # Function to install GPU packages
 install_gpu_package() {
-    local pacman_gpu_packages=(
-        "vulkan-radeon" "lib32-vulkan-radeon"
-        "lib32-mesa" "lib32-mesa-demos" "lib32-mesa-vdpau" "lib32-vulkan-radeon" 
-        "libva-mesa-driver" "mesa" "mesa-vdpau" "vulkan-radeon" "xf86-video-amdgpu"
-    )
+    local gpu_packages=()
+
+    # Read packages from the packages_gpu file, skipping empty lines
+    while IFS= read -r package; do
+        # Check if the line is not empty
+        if [[ -n "$package" ]]; then
+            gpu_packages+=("$package")
+        fi
+    done < "./install/packages_gpu"
 
     if lsmod | grep -q '^amdgpu\s'; then
-        local total_packages=${#pacman_gpu_packages[@]}
+        local total_packages=${#gpu_packages[@]}
 
-        echo "${COLOR_GREEN}:: GPU packages to be installed - Package (${total_packages})${COLOR_RESET}"
+        echo "${COLOR_GREEN}:: GPU packages to be installed${COLOR_RESET}"
 
-        for package in "${pacman_gpu_packages[@]}"; do
-            echo "${COLOR_GREY}$package${COLOR_RESET}"
-        done
+        printf "${COLOR_GREY}%s${COLOR_RESET}\n" "${gpu_packages[@]}" | column
 
-        if prompt_yna ":: Install these GPU pacman packages?"; then
-            sudo pacman -S "${pacman_gpu_packages[@]}"
+        if prompt_yna ":: Install these GPU  packages? - Total Package (${total_packages})"; then
+            paru -S "${gpu_packages[@]}"
 
             sleep 1
             clear
@@ -65,30 +60,6 @@ install_gpu_package() {
             sleep 1
             clear
         fi
-    else
- echo "${COLOR_YELLOW}:: AMDGPU driver not detected. These packages are typically used with AMD GPUs. Installation aborted.${COLOR_RESET}"
-    fi
-}
-
-# Function to install AUR packages
-install_aur_packages() {
-    local aur_packages=("hyprshot" "hyprswitch" "hyprpicker" "swww" "wlogout" "emote" "woomer" "bibata-cursor-theme" "ttf-ds-digital")
-    echo "${COLOR_GREEN}:: AUR packages to be installed:${COLOR_RESET}"
-
-    for package in "${aur_packages[@]}"; do
-        echo "${COLOR_GREY}$package${COLOR_RESET}"
-    done
-
-    if prompt_yna ":: Install these AUR packages?"; then
-        yay -S "${aur_packages[@]}"
-
-        sleep 1
-        clear
-    else
-      echo "${COLOR_YELLOW}:: Skipping AUR package installation.${COLOR_RESET}"
-
-      sleep 1
-      clear
     fi
 }
 
@@ -96,23 +67,21 @@ install_aur_packages() {
 display_banner() {
     echo -e "${COLOR_GRAY}---------------------------------------------------------${COLOR_RESET}"
     echo -e "${COLOR_BLUE}Part 1: Installing Required Packages${COLOR_RESET}"
-    echo -e "${COLOR_BLUE}This part will install the necessary packages from pacman and AUR.${COLOR_RESET}"
-    echo -e "${COLOR_BLUE}Packages are categorized into tool packages, GPU packages, and AUR packages (yay).${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}This part will install the necessary packages.${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}Packages are categorized into tool packages and GPU packages.${COLOR_RESET}"
     echo -e "${COLOR_BLUE}You will be prompted to choose whether to proceed with each installation.${COLOR_RESET}"
+    echo -e "${COLOR_BLUE}This will be look long time to complete, since for the AUR package installation was using source code to compile instead of binary.${COLOR_RESET}"
     echo -e "${COLOR_BLUE}Type 'n' or 'no' if you wish to skip a package.${COLOR_RESET}"
     echo -e "${COLOR_GRAY}---------------------------------------------------------${COLOR_RESET}"
 }
 
-main(){
+main() {
     clear
     # Display the banner
     display_banner
 
-    # Install pacman packages
-    install_pacman_packages
-
-    # Install AUR packages
-    install_aur_packages
+    # Install main packages
+    install_main_packages
 
     # Install GPU packages
     install_gpu_package
