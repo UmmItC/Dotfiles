fbbec02eba1e5e15ed42259e99733bf32a87bc82
UmmIt <git@ummit.dev>
2025-05-15 14:00:10 +0800

    fix(hyprswitch): update restart script for improve logging

--- script/misc/restart/hyprswitch-restart.sh
+++ script/misc/restart/hyprswitch-restart.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-start_hyprswitch="hyprswitch init --show-title --size-factor 6.5 --custom-css /home/leon/.config/hypr/hyprswitch/style.css"
+start_hyprswitch="hyprswitch init --show-title --size-factor 6.5 --custom-css $HOME/.config/hypr/hyprswitch/style.css"
 
-# Log path
-log_file=~/script/hypr/hyprswitch/hyprswitch-restart.log
+LOG_DIR="$HOME/.local/share/hyprswitch"
+log_file="$LOG_DIR/hyprswitch-restart.log"
 
-# Ensure the directory exists
-if mkdir -p "$LOG_DIR"; then
-    echo "Directory $LOG_DIR created or already exists."
-else
-    echo "Failed to create $LOG_DIR."
+if [ ! -d "$LOG_DIR" ]; then
+    if mkdir -p "$LOG_DIR"; then
+        echo "Directory $LOG_DIR created."
+    else
+        echo "Failed to create $LOG_DIR."
+    fi
 fi
 
-# Check if hyprswitch is running
 if pgrep -x "hyprswitch" > /dev/null; then
     echo "Hyprswitch is running. Restarting..."
     killall hyprswitch
-    nohup $start_hyprswitch 2>&1 &
-    hyprctl notify 1 2500 "rgb(ED5AB3)" "fontsize:35   Hyprswitch has been restarted."
-    echo "Hyprswitch has been restarted." >> $log_file
+    nohup $start_hyprswitch > /dev/null 2>&1 &
+    hyprctl dispatch notify 3 5000 "Hyprswitch" "Restarted"
+    echo "$(date): Hyprswitch has been restarted." >> "$log_file"
 else
     echo "Hyprswitch is not currently running"
-    nohup $start_hyprswitch 2>&1 &
-    hyprctl notify 1 2500 "rgb(ED5AB3)" "fontsize:35   Hyprswitch has been started."
+    nohup $start_hyprswitch > /dev/null 2>&1 &
+    hyprctl dispatch notify 3 5000 "Hyprswitch" "Started"
+    echo "$(date): Hyprswitch has been started." >> "$log_file"
 fi
